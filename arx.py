c672b3a0030090fb61763049067d298eccc4a707
Yilun Guan <zoom.aaron@gmail.com>
2018-11-05 17:42:55 -0500

    add main script

---
+++ arx.py
@@ -0,0 +1,26 @@
+"""Arxiv module for Sopel
+
+Add a simple wrapper around python arxiv api wrapper
+and allows sopel bot to search on arxiv
+"""
+
+import arxiv
+from sopel import module
+from dateutil import parser
+
+@module.commands('arx')
+def arxiv_handler(bot, trigger):
+    search_query = trigger.group(2)
+    results = arxiv.query(search_query=search_query)
+    print_search_results(bot, results)
+
+
+def print_search_results(bot, records):
+    for record in records:
+        updated = record['updated']
+        updated_date = parser.parse(updated)
+        updated_date_str = updated_date.strftime('%Y-%m-%d')
+        outlines = "[%s] %s -- %s" % (update_date_str, \
+                                      record['title'], \
+                                      record['id'])
+        bot.say(outlines)
